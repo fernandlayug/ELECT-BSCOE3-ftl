f803841485443a3d35d2c595a6382336392e75fd
Fernand Layug <layug.fernand@auf.edu.ph>
2021-04-07 18:52:21 +0800

    addiional activity

--- Module02/mod2-test.php
+++ Module02/mod2-test.php
@@ -1,9 +1,110 @@
 <!DOCTYPE html >
 <head>
-<title>Logical Examples</title>
+<title>Functions</title>
 </head>
 <body>
 <?php
+
+/*
+function name_of_function(parameters)
+{ 
+    statements;
+}
+*/
+
+
+/*
+function displayCourse($Course1, $Course2, $Course3)
+
+{  
+    echo "Courses Offered" . BR;
+    echo "Computer Studies: " . $Course1 . BR;
+    echo $Course2 . BR;
+    echo $Course3 . BR; 
+}
+
+displayCourse("BSIS", "BSIT", "BSCS");
+
+function displayCourse2 ($Course1, $Course2, $Course3)
+{
+    return $Course1;
+    return $Course2;
+    return $Course3;
+
+    echo BR;
+}
+
+echo displayCourse2("BSIS", "BSIT", "BSCS");
+echo BR;
+
+function averageNumber($a, $b, $c)
+{
+    $SumofNumbers = $a + $b + $c;
+    $result = $SumofNumbers / 3;
+
+    return $result;
+   
+}
+echo averageNumber(90, 98, 85);
+
+*/
+
+/*
+$a = 50;
+$b = 25;
+
+function addition()
+{
+$GLOBALS['c'] = $GLOBALS ['a'] + $GLOBALS['b'];
+}
+
+addition();
+echo $c;
+*/
+/*
+if( Conditional Statement)
+{
+Statement
+}
+
+*/
+
+// $Value =  70;
+
+// if ($Value >= 75)
+
+// {
+//     echo "<p> PASSED</p>";
+
+//     $a = 5;
+//     $b = 10;
+
+//     $c = $a + $b;
+
+//     echo $c;
+
+// }
+
+//      else
+
+// {
+//     echo "<p> FAILED.</p>";
+
+
+// }
+
+// echo "<p> This statement always executes after the if statement.</p>";
+// $oldpassword = "a";
+// $password = "a";
+
+// $new_password = "aa";
+// $confirm_password = "aa";
+// if($oldpassword == $password)
+// if($new_password == $confirm_password)
+// {
+//     echo "Change Password accepted";
+// }
+
 
 ?>
 </body>
