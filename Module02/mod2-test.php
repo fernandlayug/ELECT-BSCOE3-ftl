70f5f42e5d03c5eabbd76dcf3f58c5c20e1b5755
Fernand Layug <layug.fernand@auf.edu.ph>
2021-04-07 19:41:48 +0800

    additional activity

--- Module02/mod2-test.php
+++ Module02/mod2-test.php
@@ -94,17 +94,74 @@ Statement
 // }
 
 // echo "<p> This statement always executes after the if statement.</p>";
+
 // $oldpassword = "a";
 // $password = "a";
 
 // $new_password = "aa";
 // $confirm_password = "aa";
+
 // if($oldpassword == $password)
 // if($new_password == $confirm_password)
 // {
 //     echo "Change Password accepted";
 // }
-echo "hello";
+
+// $SalesTotal = 40;
+// if ($SalesTotal >= 50)
+// if ($SalesTotal <=100)
+// echo "The Sales Total is between 50 and 100.";
+// $age = 33;
+// if ($age < 30)
+// {
+//     // code is executed if the expression1 evaluates TRUE
+//     echo "Your age is less than 30!";
+// }
+// elseif ($age > 30 && $age <40)
+// {
+//     // code is executed if the expression2 evaluates TRUE
+//     echo "Your age is between 30 and 40";
+// }
+// elseif ($age > 40 && $age < 50)
+// {
+//     // code is executed if the expression3 evaluates TRUE
+//     echo "Your age is between 40 and 50";
+// }
+// else
+// {
+//     //code is executed if the the expression1, expression2, expression3 evaluates to FALSE
+//     echo "Your age is greater 50";
+// }
+$site = "Business";
+switch ($site)
+{
+    case 'Business':
+        echo "My favourite site is business.com!";
+        $a = 55;
+        $b = 10;
+        $c = $a + $b;
+        echo $c;
+        break;
+
+    case 'Code':
+        echo "My favourite site is code.com!";
+        break;
+
+    case 'Music':
+        echo "My favourite site is music.com!";
+        break;
+        
+    case 'Video':
+        echo "My favourite site is video.com!";
+        break;
+        
+    case 'Photography':
+        echo "My favourite site is photography.com!";
+        break;
+
+    default:
+    echo "I like everything at site";
+}
 
 ?>
 </body>
